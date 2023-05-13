583440db8f7cdc071b2edc6dfd592476198e3b03
Kevin Galarza Vera <44540130+kgalarza95@users.noreply.github.com>
2023-05-13 18:56:34 -0500

    xx

---
+++ src/main/java/ver/galarza/kvn/SBApiJdk8/config/RestDocsConfig.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package ver.galarza.kvn.SBApiJdk8.config;
+
+/**
+ *
+ * @author kgalarza
+ */
+public class RestDocsConfig {
+    
+}
