73e02261637412a4228d97d5a02774a19e76023b
Kevin Galarza Vera <44540130+kgalarza95@users.noreply.github.com>
2023-05-12 21:21:39 -0500

    se agrega dependencias de swagger

--- src/test/java/ver/galarza/kvn/SBApiJdk8/SbApiJdk8ApplicationTests.java
+++ src/test/java/ver/galarza/kvn/SBApiJdk8/SbApiJdk8ApplicationTests.java
@@ -3,11 +3,11 @@ package ver.galarza.kvn.SBApiJdk8;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+//@SpringBootTest
 class SbApiJdk8ApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+//	@Test
+//	void contextLoads() {
+//	}
 
 }
